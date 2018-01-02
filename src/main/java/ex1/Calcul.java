b62c296f73ba1dbbb6ca5303a45a6934932ca065
ETY15 <ETY15@DESKTOP-ABI8QL4>
2018-01-02 14:57:37 +0100

    Exercice 1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,26 @@
+package ex1;
+
+/**
+ * @author Leila
+ *
+ */
+public class Calcul {
+
+	/** Méthode qui retourne l'addition des paramètres a et b
+	 * @param a paramètre a
+	 * @param b paramètre b
+	 * @return int
+	 */
+	public int additionner(int a, int b){
+		return a+b;
+	}
+	
+	/** Méthode qui retourne la soustraction de b à a
+	 * @param a paramètre a
+	 * @param b paramètre b
+	 * @return int
+	 */
+	public int soustraire(int a, int b){
+		return a-b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,30 @@
+
+package ex1;
+
+import java.util.Date;
+
+/**
+ * @author Leila
+ *
+ */
+public class Entreprise {
+
+	
+	/** Numéro siret de l'entreprise */
+	public int siret;
+	/** Nom de l'entreprise */
+	public String nom;
+	/**Adresse de l'entreprise */
+	public String adresse;
+	/** Date de creation de l'entreprise */
+	public Date dateCreation;
+	
+	/** Capital maximal que peut avoir l'entreprise */
+	public static final int capitalMax = 3000000;
+	
+	/** Fonction non terminée étant censée afficher les informations sur l'entreprise. Elle se nommait afficher_statut ce qui n'est pas recommandable */
+	public void afficher_info(){
+		
+	}
+	
+}
